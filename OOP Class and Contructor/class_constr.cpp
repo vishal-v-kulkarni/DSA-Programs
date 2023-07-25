de54ad64d45a9712918743dac1b05fb1e9bc936d
Vishal Kulkarni <vkvishal1508@gmail.com>
2023-07-24 21:39:12 -0400

    Class and Contructor

--- OOP Class and Contructor/class_constr.cpp
+++ OOP Class and Contructor/class_constr.cpp
@@ -2,3 +2,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
+
+class Rectangle{
+    
+    private:
+    int length;
+    int breadth;
+
+    public:
+    Rectangle(int l, int b){
+        length = l;
+        breadth = b;
+    }
+
+    int area(){
+        return length * breadth;
+    }
+
+    void changeLength(int l){
+        length = l;
+    }
+
+};
+
+int main(){
+    
+    Rectangle r(10,5);
+
+    int ar = r.area();
+    r.changeLength(15);
+
+
+    return 0;
+}
